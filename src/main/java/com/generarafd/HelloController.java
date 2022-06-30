c493215e39fecb19ca931ae65c9df72bfc2a81f1
joseop <josec.ortiz@udea.edu.co>
2022-06-29 21:45:36 -0500

    view project

--- src/main/java/com/generarafd/HelloController.java
+++ src/main/java/com/generarafd/HelloController.java
@@ -2,13 +2,80 @@ package com.generarafd;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextArea tAAFDM;
+
+    @FXML
+    private TextArea tAAFResultante;
+
+    @FXML
+    private TextArea tAAsignarVariables;
+
+    @FXML
+    private TextArea tACierreLambda;
+
+    @FXML
+    private TextArea tAEstadosResultantes;
+
+    @FXML
+    private TextArea tAEvaluarCierreLambda;
+
+    @FXML
+    private TextArea tAGrupos;
+
+    @FXML
+    private TextArea tATransiciones;
+
+    @FXML
+    private Tab tabAsignarVariables;
+
+    @FXML
+    private Tab tabCierreLambda;
+
+    @FXML
+    private Tab tabNuevosEstados;
+
+    @FXML
+    private Tab tabTransiciones;
+
+    @FXML
+    private Label txtNota;
+
+    @FXML
+    void Transiciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void asignarVariables(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCerrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnConvertir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cierreLambda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevosEstados(ActionEvent event) {
+
     }
 }
