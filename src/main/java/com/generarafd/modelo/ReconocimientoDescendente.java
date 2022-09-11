4e51daeceb65777b3e03e1d04ff9a01b4cf0006e
joseop <josec.ortiz@udea.edu.co>
2022-09-11 11:53:24 -0500

    Mensajes de error

--- src/main/java/com/generarafd/modelo/ReconocimientoDescendente.java
+++ src/main/java/com/generarafd/modelo/ReconocimientoDescendente.java
@@ -2,7 +2,6 @@ package com.generarafd.modelo;
 
 import javax.swing.JOptionPane;
 
-import java.awt.*;
 import java.util.Stack;
 
 public class ReconocimientoDescendente {
@@ -10,7 +9,7 @@ public class ReconocimientoDescendente {
     private Stack<String> pila = new Stack<>();
     private String simbolo;
     private int x = 0;
-    private boolean salir = false, correcto=false;
+    private boolean salir = false, correcto = false;
 
     //Constructor
     public ReconocimientoDescendente(String er) {
@@ -26,22 +25,32 @@ public class ReconocimientoDescendente {
             switch (pila.lastElement()) {
                 case "A":
                     switch (simbolo) {
-                        case "|":salir=true;
-                            break;
-                        case ".":salir=true;
+                        case "|":
+                        case ".":
+                            JOptionPane.showMessageDialog(null, "Hay un operador al inicio de la ER",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case "(":
                         case "1":
                         case "0":
                             remplazar("BC");
                             break;
-                        case ")":salir=true;
+                        case ")":
+                            JOptionPane.showMessageDialog(null, "Hay un abre parentesis al inicio de la ER",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "*":salir=true;
+                        case "*":
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "Hay un operador elevado al inicio de la ER",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
-                            break;
-                        case "-":salir=true;
+                        case "-":
+                            JOptionPane.showMessageDialog(null, "No usar el simbolo fin se secuencua -",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
@@ -51,63 +60,99 @@ public class ReconocimientoDescendente {
                             remplazar("BC");
                             avance();
                             break;
-                        case ".":salir=true;
+                        case ".":
+                            JOptionPane.showMessageDialog(null, "7",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "(":salir=true;
+                        case "(":
+                            JOptionPane.showMessageDialog(null, "8",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case ")":
                         case "-":
                             desapilar();
                             break;
-                        case "0":salir=true;
+                        case "0":
+                            JOptionPane.showMessageDialog(null, "9",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "1":salir=true;
+                        case "1":
+                            JOptionPane.showMessageDialog(null, "10",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "*":salir=true;
+                        case "*":
+                            JOptionPane.showMessageDialog(null, "11",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "12",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
                 case "C":
                     switch (simbolo) {
-                        case "|":salir=true;
-                            break;
-                        case ".":salir=true;
+                        case "|":
+                        case ".":
+                            salir = true;
                             break;
                         case "(":
                         case "0":
                         case "1":
                             remplazar("ED");
                             break;
-                        case ")":salir=true;
+                        case ")":
+                            JOptionPane.showMessageDialog(null, "Hay un cierre parentesis despues de un operador",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "*":salir=true;
+                        case "*":
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "Hay un operador elevado despues de un operador",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
-                            break;
-                        case "-":salir=true;
+                        case "-":
+                            JOptionPane.showMessageDialog(null, "No usar el simbolo (-) fin de secuencia o verificar operador al final de la ER",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
                 case "D":
                     switch (simbolo) {
-                        case "|":salir=true;
-                            break;
-                        case ".":salir=true;
+                        case "|":
+                        case ".":
+                            JOptionPane.showMessageDialog(null, "Hay dos operadores seguidos",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case "(":
                         case "1":
                         case "0":
                             remplazar("GF");
                             break;
-                        case ")":salir=true;
+                        case ")":
+                            JOptionPane.showMessageDialog(null, "Hay un cierre parentesis despues de un operador",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "*":salir=true;
+                        case "*":
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "Hay un operador elevado despues de un operador",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
-                            break;
-                        case "-":salir=true;
+                        case "-":
+                            JOptionPane.showMessageDialog(null, "No usar el simbolo (-) fin de secuencia o verificar operador al final de la ER",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
@@ -122,40 +167,65 @@ public class ReconocimientoDescendente {
                             remplazar("ED");
                             avance();
                             break;
-                        case "(":salir=true;
+                        case "(":
+                            JOptionPane.showMessageDialog(null, "Hay un parentesis despues de un operador elevado, usar operadores para separar",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "0":salir=true;
+                        case "0":
+                        case "1":
+                            JOptionPane.showMessageDialog(null, "Hay un operando despues de un operador elevado, usar operadores para separar",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "1":salir=true;
-                            break;
-                        case "*":salir=true;
-                            break;
-                        case "+":salir=true;
+                        case "*":
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "Hay dos operadores elevados seguidos",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
                 case "F":
                     switch (simbolo) {
-                        case "|":salir=true;
+                        case "|":
+                            JOptionPane.showMessageDialog(null, "30",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case ".":salir=true;
+                        case ".":
+                            JOptionPane.showMessageDialog(null, "31",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case "(":
                             remplazar(")A");
                             avance();
                             break;
-                        case ")":salir=true;
+                        case ")":
+                            JOptionPane.showMessageDialog(null, "32",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case "0":
                         case "1":
                             desapilar();
                             avance();
                             break;
-                        case "*":salir=true;
+                        case "*":
+                            JOptionPane.showMessageDialog(null, "33",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "34",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "-":salir=true;
+                        case "-":
+                            JOptionPane.showMessageDialog(null, "35",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
@@ -167,11 +237,16 @@ public class ReconocimientoDescendente {
                         case "-":
                             desapilar();
                             break;
-                        case "(":salir=true;
+                        case "(":
+                            JOptionPane.showMessageDialog(null, "Hay un parentesis despues de un operando, poner un operador",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "0":salir=true;
-                            break;
-                        case "1":salir=true;
+                        case "0":
+                        case "1":
+                            JOptionPane.showMessageDialog(null, "Existen mas de 2 operandos seguidos",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case "*":
                         case "+":
@@ -182,48 +257,96 @@ public class ReconocimientoDescendente {
                     break;
                 case ")":
                     switch (simbolo) {
-                        case "|":salir=true;
+                        case "|":
+                            JOptionPane.showMessageDialog(null, "39",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case ".":salir=true;
+                        case ".":
+                            JOptionPane.showMessageDialog(null, "40",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "(":salir=true;
+                        case "(":
+                            JOptionPane.showMessageDialog(null, "41",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case ")":
                             desapilar();
                             avance();
                             break;
-                        case "0":salir=true;
+                        case "0":
+                            JOptionPane.showMessageDialog(null, "42",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "1":salir=true;
+                        case "1":
+                            JOptionPane.showMessageDialog(null, "43",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "*":salir=true;
+                        case "*":
+                            JOptionPane.showMessageDialog(null, "44",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "45",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "-":salir=true;
+                        case "-":
+                            JOptionPane.showMessageDialog(null, "46",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                     }
                     break;
                 case "λ":
                     switch (simbolo) {
-                        case "|":salir=true;
+                        case "|":
+                            JOptionPane.showMessageDialog(null, "47",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case ".":salir=true;
+                        case ".":
+                            JOptionPane.showMessageDialog(null, "48",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "(":salir=true;
+                        case "(":
+                            JOptionPane.showMessageDialog(null, "49",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case ")":salir=true;
+                        case ")":
+                            JOptionPane.showMessageDialog(null, "Hay un abre parentesis de más",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "0":salir=true;
+                        case "0":
+                            JOptionPane.showMessageDialog(null, "51",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "1":salir=true;
+                        case "1":
+                            JOptionPane.showMessageDialog(null, "52",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "*":salir=true;
+                        case "*":
+                            JOptionPane.showMessageDialog(null, "53",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
-                        case "+":salir=true;
+                        case "+":
+                            JOptionPane.showMessageDialog(null, "54",
+                                    "Verificar Error", JOptionPane.ERROR_MESSAGE);
+                            salir = true;
                             break;
                         case "-":
-                            correcto=true;
+                            correcto = true;
                             salir = true;
                             break;
                     }
